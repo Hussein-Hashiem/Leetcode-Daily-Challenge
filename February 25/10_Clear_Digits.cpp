610da5c24343701499b81b2bf62104643adce5db
Hussein Hashiem <husshashiem@gmail.com>
2025-02-10 11:51:29 +0200

    The daily problem has been added.

---
+++ February 25/10_Clear_Digits.cpp
@@ -0,0 +1,26 @@
+/*
+Problem Link
+https://leetcode.com/problems/clear-digits/description/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+class Solution
+{
+public:
+    string clearDigits(string s)
+    {
+        string ans = "";
+        int n = s.size();
+        stack<char> st;
+        for (int i = 0; i < n; i++)
+            (s[i] >= '0' && s[i] <= '9') ? st.pop() : st.push(s[i]);
+        while (!st.empty())
+            ans = st.top() + ans, st.pop();
+        return ans;
+    }
+};
+int main()
+{
+    return 0;
+}
