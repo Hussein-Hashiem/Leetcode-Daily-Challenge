d3ea6eb8fe5c4249c27c1b06c51e524a42272a81
Hussein Hashiem <husshashiem@gmail.com>
2025-02-19 17:12:16 +0200

    day 19

--- February 25/19_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
+++ February 25/19_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
@@ -7,20 +7,22 @@ using namespace std;
 class Solution
 {
 public:
-    int vis[3];
-    vector<string> ans;
-    void solve(int idx, int n, string curr)
+    int vis[3], cnt;
+    string ans = "";
+    void solve(int idx, int n, string curr, int k)
     {
         if (idx == n)
         {
-            ans.push_back(curr);
+            cnt++;
+            if (cnt == k)
+                ans = curr;
             return;
         }
         if (!idx)
             for (int i = 0; i < 3; i++)
             {
                 vis[i]++;
-                solve(idx + 1, n, curr + char('a' + i));
+                solve(idx + 1, n, curr + char('a' + i), k);
                 vis[i]--;
             }
         else
@@ -29,15 +31,15 @@ public:
                 if (curr[idx - 1] != 'a' + i)
                 {
                     vis[curr[idx - 1] - 'a']--, vis[i]++;
-                    solve(idx + 1, n, curr + char('a' + i));
+                    solve(idx + 1, n, curr + char('a' + i), k);
                     vis[i]--, vis[curr[idx - 1] - 'a']++;
                 }
             }
     }
     string getHappyString(int n, int k)
     {
-        solve(0, n, "");
-        return k <= ans.size() ? ans[k - 1] : "";
+        solve(0, n, "", k);
+        return ans;
     }
 };
 int main()
