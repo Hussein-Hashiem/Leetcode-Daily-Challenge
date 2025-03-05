f200e79d66e6c0740300ccf52f6db8c23214d0df
Hussein Hashiem <husshashiem@gmail.com>
2025-03-05 14:22:03 +0200

    day 5

--- March 25/05_Count_Total_Number_of_Colored_Cells.cpp
+++ March 25/05_Count_Total_Number_of_Colored_Cells.cpp
@@ -4,17 +4,10 @@ https://leetcode.com/problems/count-total-number-of-colored-cells/description/
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 class Solution
 {
 public:
-    ll coloredCells(int n)
-    {
-        ll ans = 1;
-        for (int i = 1; i < n; i++)
-            ans += (i * 4);
-        return ans;
-    }
+    long long coloredCells(int n) { return 1 + 1LL * 4 * n * (n - 1) / 2; }
 };
 int main()
 {
